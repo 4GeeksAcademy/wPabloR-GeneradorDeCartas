41f49db7b184c18fda8bdacaa3278bf872262fe6
wPabloR <ricotep0@gmail.com>
2025-05-25 19:19:10 +0000

    Creación de carta + botón

--- src/app.js
+++ src/app.js
@@ -5,7 +5,40 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  const suits = ["♦", "♥", "♠", "♣"];
+
+  const topSuit = document.querySelector("#topSuit");
+  const numb = document.querySelector("#number");
+  const bottomSuit = document.querySelector("#bottomSuit");
+  const cardElement = document.querySelector(".card");
+  const btn = document.querySelector("#btn");
+
+
+
+  function toggleCard() {
+    const randomNumb = Math.floor(Math.random() * numbers.length);
+    const randomSuits = Math.floor(Math.random() * suits.length);
+    const selectedSuit = suits[randomSuits];
+    
+    const suitClasses = {
+      "♦": "diamond",
+      "♥": "heart",
+      "♠": "spade",
+      "♣": "club"
+    };
+    
+    topSuit.innerHTML = selectedSuit;
+    numb.innerHTML = numbers[randomNumb];
+    bottomSuit.innerHTML = selectedSuit;
+
+    cardElement.classList.remove("diamond", "heart", "spade", "club");
+    cardElement.classList.add(suitClasses[selectedSuit])
+  }
+
+  toggleCard();
+
+btn.addEventListener("click", toggleCard)
+
 };
